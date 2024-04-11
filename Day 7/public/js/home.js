da2053a265b482a9f023aaf211665dd7b0234c8c
Reuben27 <reuben.sd@iitgn.ac.in>
2024-04-11 23:00:07 +0530

    Upload Codes

---
+++ Day 7/public/js/home.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('post-btn');
+
+button.addEventListener('click', function(event) {
+  try {     
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    console.log(name);
+    var subscribe = document.getElementById("subscribe").value;
+    console.log(subscribe);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const raw = JSON.stringify({
+      "name": name,
+      "subscribedToChannel": subscribe
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    fetch("http://localhost:3000/subscribers", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  } catch(err) {
+    console.error(`Error: ${err}`);
+  }
+});
